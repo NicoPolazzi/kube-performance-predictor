7a854688d16575ab911ef7d692cd76e18650b112
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-11-11 18:42:10 +0100

    Implement get_average_response_time

--- src/kube_performance_predictor/collector.py
+++ src/kube_performance_predictor/collector.py
@@ -1,11 +1,11 @@
 from prometheus_api_client import PrometheusConnect
 
 
-def get_average_response_time(prometheus_url: str) -> float:
-    prom = PrometheusConnect(url=prometheus_url)
+def get_average_response_time(service_name: str) -> float:
+    prom = PrometheusConnect("http://localhost:9090")
 
     data = prom.custom_query(
-        query='sum(rate(istio_request_duration_milliseconds_sum{destination_workload=~"frontend",destination_workload_namespace="default"}[1m]))/sum(rate(istio_request_duration_milliseconds_count{destination_workload=~"frontend",destination_workload_namespace="default"}[1m])) / 1000'
+        query=f'sum(rate(istio_request_duration_milliseconds_sum{{destination_workload=~"{service_name}",destination_workload_namespace="default"}}[1m]))/sum(rate(istio_request_duration_milliseconds_count{{destination_workload=~"{service_name}",destination_workload_namespace="default"}}[1m])) / 1000'
     )
 
     value_str = data[0]["value"][1]
