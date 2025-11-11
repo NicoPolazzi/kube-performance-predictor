142a1dc8a734070efa10d0b760f1200374ac98c0
NicoPolazzi <nicolo.polazzi@gmail.com>
2025-11-11 19:19:05 +0100

    Implement get_cpu_usage

--- src/kube_performance_predictor/collector.py
+++ src/kube_performance_predictor/collector.py
@@ -1,23 +1,58 @@
+import math
 from prometheus_api_client import PrometheusConnect
+
+prom = PrometheusConnect("http://localhost:9090")
+VALUE_KEY = "value"
 
 
 def get_average_response_time(service_name: str) -> float:
-    prom = PrometheusConnect("http://localhost:9090")
+    """
+    get_average_response_time returns the response time in seconds of a service_name quering the Prometheus server.
 
-    data = prom.custom_query(
+    We are assuming that all our services resides in the default namespace.
+    """
+    result = prom.custom_query(
         query=f'sum(rate(istio_request_duration_milliseconds_sum{{destination_workload=~"{service_name}",destination_workload_namespace="default"}}[1m]))/sum(rate(istio_request_duration_milliseconds_count{{destination_workload=~"{service_name}",destination_workload_namespace="default"}}[1m])) / 1000'
     )
 
-    value_str = data[0]["value"][1]
+    if not result:
+        return math.nan
+
+    value_str = result[0][VALUE_KEY][1]
     return float(value_str)
 
 
 def get_throughtput(service_name: str) -> float:
-    prom = PrometheusConnect("http://localhost:9090")
+    """
+    get_throughput returns the throughtput of a service_name quering the Prometheus server.
 
-    data = prom.custom_query(
-        query=f'sum(rate(istio_requests_total{{destination_workload=~"{service_name}"}}[5m]))'
+    We are assuming that all our services resides in the default namespace.
+    """
+
+    result = prom.custom_query(
+        query=f'sum(rate(istio_requests_total{{destination_workload=~"{service_name}", destination_workload_namespace="default"}}[5m]))'
     )
 
-    value_str = data[0]["value"][1]
+    if not result:
+        return math.nan
+
+    value_str = result[0][VALUE_KEY][1]
+    return float(value_str)
+
+
+def get_cpu_usage(service_name: str) -> float:
+    """
+    get_cpu_usage returns the cpu usage of the pods running a service_name quering the Prometheus server.
+
+    We are assuming that all our services resides in the default namespace.
+    """
+
+    result = prom.custom_query(
+        query=f'sum(rate(container_cpu_usage_seconds_total{{pod=~"{service_name}-.*", destination_workload_namespace="default"}}[5m]))'
+    )
+
+    if not result:
+        return math.nan
+
+    value_str = result[0][VALUE_KEY][1]
     return float(value_str)
